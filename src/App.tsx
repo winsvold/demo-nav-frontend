763ba28a65fa59ee9bf363c4f53262862f5012b0
Daniel Winsvold <danielwinsvold@gmail.com>
2019-06-21 14:13:30 +0200

    kode fra live-demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.less';
+import {Normaltekst, Sidetittel} from "nav-frontend-typografi";
+import Panel from "nav-frontend-paneler";
+import {Hovedknapp} from "nav-frontend-knapper";
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>Designsystemet</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Panel border={true}>
+                <Sidetittel>Designsystemet</Sidetittel>
+                <Normaltekst>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, iure!</Normaltekst>
+            </Panel>
+            <Panel>
+                <Hovedknapp onClick={() => alert('hei')}>Send inn søknad</Hovedknapp>
+            </Panel>
+        </div>
+    );
 };
 
 export default App;
